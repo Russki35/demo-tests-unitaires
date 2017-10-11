bdfadbe63a2e9cb15d872c559c9fa4f36d4e198d
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-11 15:08:19 +0200

    log réparé

---
+++ src/main/java/dev/service/CalculService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dev.service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * @author ETY8
+ *
+ */
+public class CalculService {
+	
+	public int additionner(String expression) {
+		// TODO Ajouter un log en niveau DEBUG affichant "Evaluation de l'expression
+		//<expression>".
+		// TODO par exemple "Evaluation de l'expression 1+4"
+		return 0;
+		}
+}
---
+++ src/main/java/dev/service/CalculServiceTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package dev.service;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test unitaire de la classe dev.service.CalculService.
+ */
+public class CalculServiceTest {
+	private static final Logger LOG = LoggerFactory.getLogger(CalculServiceTest.class);
+
+	@Test
+	public void testAdditionner() throws Exception {
+		LOG.info("Etant donné, une instance de la classe CalculService");
+		// TODO
+		LOG.info("Lorsque j'évalue l'addition de l'expression 1+3+4");
+		// TODO
+		int somme = 0;
+		LOG.info("Alors j'obtiens le résultat 8");
+		// TODO
+	}
+}
